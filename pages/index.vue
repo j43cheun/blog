905c226a80ba41cd5f4eb1cc8e7384744c07f93c
Justin Cheung <j43cheun@uwaterloo.ca>
2021-02-08 00:13:26 -0800

    Moved pikachu.png into png folder under assets folder

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,21 @@
   <section class="hero is-halfheight">
     <div class="hero-body">
       <div class="container has-text-centered">
-          <figure class="image container is-128x128 mb-6">
-            <img src="~/assets/pikachu.png">
-          </figure>
+        <figure class="image container is-128x128 mb-6">
+          <img src="~/assets/png/pikachu.png">
+        </figure>
         <p class="title">
           Justin Cheung
         </p>
         <p class="subtitle">
           Hi! I'm a software developer.
         </p>
-        <b-button tag="router-link" to="/about" type="is-link">About Me</b-button>
-        <b-button tag="router-link" to="/projects" type="is-link">My Projects</b-button>
+        <b-button tag="router-link" to="/about" type="is-link">
+          About Me
+        </b-button>
+        <b-button tag="router-link" to="/projects" type="is-link">
+          My Projects
+        </b-button>
       </div>
     </div>
   </section>
