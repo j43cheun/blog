6e65f8c4930573f5e85cac7c2af6206df4ed44dd
Justin Cheung <j43cheun@uwaterloo.ca>
2021-02-07 19:31:02 -0800

    Added Navbar and Footer components

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <div>
+        <code>echo "Hello world!"</code>
+      </div>
+      <div class="is-size-7">
+        &copy; Justin Cheung {{ new Date().getFullYear() }}
+      </div>
+    </div>
+  </footer>
+</template>
