d134405e6a7d78d4de153b048ecb5222a3569f9c
Justin Cheung <j43cheun@uwaterloo.ca>
2021-02-07 21:16:27 -0800

    Added hero to index.vue page (#3)

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,19 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+  <section class="hero is-halfheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+          <figure class="image container is-128x128 mb-6">
+            <img src="~/assets/pikachu.png">
+          </figure>
+        <p class="title">
+          Justin Cheung
+        </p>
+        <p class="subtitle">
+          Hi! I'm a software developer.
+        </p>
+        <b-button tag="router-link" to="/about" type="is-link">About Me</b-button>
+        <b-button tag="router-link" to="/projects" type="is-link">My Projects</b-button>
+      </div>
     </div>
   </section>
 </template>
-
-<script>
-import Card from '~/components/Card'
-
-export default {
-  name: 'HomePage',
-
-  components: {
-    Card
-  }
-}
-</script>
