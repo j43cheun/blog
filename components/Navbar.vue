ba3c2fb20bac8ae069102a404a73ae688911e38e
Justin Cheung <j43cheun@uwaterloo.ca>
2021-02-08 00:05:02 -0800

    Added About Me and Project links to Navbar (#4)

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,22 @@
 <template>
   <b-navbar spaced>
     <template #brand>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <strong>Justin Cheung</strong>
-      </b-navbar-item>
-    </template>
-    <template #start>
       <b-navbar-item href="https://github.com/j43cheun">
         <b-icon icon="github" />
       </b-navbar-item>
       <b-navbar-item href="https://www.linkedin.com/in/ulongmax/">
         <b-icon icon="linkedin" />
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <strong>Justin Cheung</strong>
+      </b-navbar-item>
+    </template>
+    <template #start>
+      <b-navbar-item tag="router-link" :to="{ path: '/about' }">
+        About Me
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: '/projects' }">
+        Projects
       </b-navbar-item>
     </template>
   </b-navbar>
