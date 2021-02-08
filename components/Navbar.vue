dcd4d6914aaf3845f96545df1f486c3c64e038d4
Justin Cheung <j43cheun@uwaterloo.ca>
2021-02-07 19:33:21 -0800

    Added Navbar and Footer components (#1)

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar spaced>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <strong>Justin Cheung</strong>
+      </b-navbar-item>
+    </template>
+    <template #start>
+      <b-navbar-item href="https://github.com/j43cheun">
+        <b-icon icon="github" />
+      </b-navbar-item>
+      <b-navbar-item href="https://www.linkedin.com/in/ulongmax/">
+        <b-icon icon="linkedin" />
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
