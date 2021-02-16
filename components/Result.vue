00f930ecd72bd1a6afef688db7039cd9b0437f84
Justin Cheung <j43cheun@uwaterloo.ca>
2021-02-16 04:42:49 -0800

    Added Blog, added My third attempt at a website blog post, and changed slug for SeeFood project post to seefood (#17)

---
+++ components/Result.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="tags mb-0">
+      <span v-for="tag of post.tags" :key="tag" class="tag">
+        {{ tag }}
+      </span>
+    </div>
+    <div>
+      <nuxt-link :to="{ name: slug, params: { slug: post.slug } }" class="is-size-4">
+        {{ post.title }}
+      </nuxt-link>
+      <p class="subtitle is-6">
+        {{ post.description }}
+      </p>
+    </div>
+    <Timestamp :created-at="post.date" />
+    <github-button
+      v-if="post.github != null"
+      :href="post.github"
+      class="mt-2"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
