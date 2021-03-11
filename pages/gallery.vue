fafddc98681fd2bab61403c2444db511c7d04a96
Justin Cheung <j43cheun@uwaterloo.ca>
2021-03-11 11:28:16 -0800

    Renamed photos directory to gallery and fixed broken image links (#24)

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -26,49 +26,49 @@ export default {
       activeIndex: 0,
       images: [
         {
-          src: '/photos/seattle_amazon_spheres_exterior.JPG'
+          src: '/gallery/seattle_amazon_spheres_exterior.JPG'
         },
         {
-          src: '/photos/redmond_construction.JPG'
+          src: '/gallery/redmond_construction.JPG'
         },
         {
-          src: '/photos/seattle_kerry_park.JPG'
+          src: '/gallery/seattle_kerry_park.JPG'
         },
         {
-          src: '/photos/uw_suzallo_library.JPG'
+          src: '/gallery/uw_suzallo_library.JPG'
         },
         {
-          src: '/photos/seattle_museum_of_flight.JPG'
+          src: '/gallery/seattle_museum_of_flight.JPG'
         },
         {
-          src: '/photos/glowing_pokeball.JPG'
+          src: '/gallery/glowing_pokeball.JPG'
         },
         {
-          src: '/photos/seattle_woodland_park_zoo_bird.JPG'
+          src: '/gallery/seattle_woodland_park_zoo_bird.JPG'
         },
         {
-          src: '/photos/seattle_woodland_park_zoo_red_panda.JPG'
+          src: '/gallery/seattle_woodland_park_zoo_red_panda.JPG'
         },
         {
-          src: '/photos/vancouver_english_bay.JPG'
+          src: '/gallery/vancouver_english_bay.JPG'
         },
         {
-          src: '/photos/poo_poo_point_paragliding.JPG'
+          src: '/gallery/poo_poo_point_paragliding.JPG'
         },
         {
-          src: '/photos/uw_campus_canada_goose.JPG'
+          src: '/gallery/uw_campus_canada_goose.JPG'
         },
         {
-          src: '/photos/seattle_king_street_station.JPG'
+          src: '/gallery/seattle_king_street_station.JPG'
         },
         {
-          src: '/photos/seattle_amazon_spheres_interior.JPG'
+          src: '/gallery/seattle_amazon_spheres_interior.JPG'
         },
         {
-          src: '/photos/marymoor_park_canada_goose.JPG'
+          src: '/gallery/marymoor_park_canada_goose.JPG'
         },
         {
-          src: '/photos/sammamish_river_trail_duck.JPG'
+          src: '/gallery/sammamish_river_trail_duck.JPG'
         }
       ]
     }
