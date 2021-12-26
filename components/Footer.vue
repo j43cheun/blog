53bf2192bdcd45ef8bc71834d906a2cb5c0341a1
Justin Cheung <j43cheun@uwaterloo.ca>
2021-12-26 03:04:23 -0800

    Added Day 659: Padoru Padoru blog post (#33)
    * Changed footer to count number of days in quarantine
    
    * Added Day 659: Padoru Padoru blog post

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="content has-text-centered is-size-7">
-      &copy; Justin Cheung {{ new Date().getFullYear() }} ⋅ Thank Mr. Goose!
+      &copy; Justin Cheung {{ new Date().getFullYear() }} ⋅ Number of days in quarantine: {{ Math.trunc((new Date() - new Date("03/06/2020")) / (1000 * 3600 * 24)) }}
     </div>
   </footer>
 </template>
